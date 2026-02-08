441c097d0e32266711220286f2dccc3b4470656e
kerosene110 <30997769+karoteeni110@users.noreply.github.com>
2026-02-08 21:13:02 +0200

    Walking skeleton: Set up server side

---
+++ walking-skeleton/server/app.js
@@ -0,0 +1,20 @@
+import { Hono } from "@hono/hono";
+import { cors } from "@hono/hono/cors";
+import { logger } from "@hono/hono/logger";
+
+const app = new Hono();
+
+app.use("/*", cors());
+app.use("/*", logger());
+
+let visits = 0;
+app.get("/api/visits", (c) => {
+  visits++;
+  return c.json({ visits });
+});
+
+app.get("/", (c) => {
+  return c.json({message: "Hello World!"});
+});
+
+export default app;
